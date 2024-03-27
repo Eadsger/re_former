a7194b56a42f330433a9bf219b610209657e0ac2
Eadsger <150641010+Eadsger@users.noreply.github.com>
2024-03-27 16:33:39 -0300

    refactor: Update UsersController and routes
    Modified UsersController to include index action and routes to include necessary actions for users. Also, added new view files for edit, index, and show actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    # Find user by ID and render show view
+  end
+
   def new
     @user = User.new
   end
@@ -7,13 +17,30 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle successful user creation
-      redirect_to users_url # Redirect to user's show page
+      redirect_to @user, notice: 'User was successfully created.'
     else
       render :new
     end
   end
 
+  def edit
+    # Render edit form
+  end
+
+  def update
+    if @user.update(user_params)
+      # Handle successful user update
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
